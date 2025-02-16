938bda9cc5962f6fc92e69e0a85b8bd2035aa00f
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-16 18:54:38 +0545

    ...

--- src/main/java/world/hello/order/controller/OrderController.java
+++ src/main/java/world/hello/order/controller/OrderController.java
@@ -1,5 +1,6 @@
 package world.hello.order.controller;
 
+import com.sun.net.httpserver.HttpsServer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,5 +21,12 @@ public class OrderController {
   public Order placeOrder(@RequestBody OrderData data) {
     log.info("POST /api/v1/orders");
     return orderService.placeOrder(data);
+  }
+
+  @GetMapping("/{orderNumber}")
+  @ResponseStatus(HttpStatus.OK)
+  public Order findOrderByOrderNumber(@PathVariable("orderNumber") final String orderNumber) {
+    log.info("GET /api/vi/orders/{}", orderNumber);
+    return orderService.fetchOrderByOrderNumber(orderNumber);
   }
 }
