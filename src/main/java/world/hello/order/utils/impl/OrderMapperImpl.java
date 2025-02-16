63170dbb0f03b94d3bb1c883865a66f12e797670
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-16 11:53:14 +0545

    logs

--- src/main/java/world/hello/order/utils/impl/OrderMapperImpl.java
+++ src/main/java/world/hello/order/utils/impl/OrderMapperImpl.java
@@ -1,39 +1,54 @@
 package world.hello.order.utils.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import world.hello.order.domain.dto.Order;
 import world.hello.order.domain.dto.OrderData;
 import world.hello.order.domain.model.OrderTable;
 import world.hello.order.utils.OrderMapper;
 
+@Slf4j
 public class OrderMapperImpl implements OrderMapper {
+
   @Override
   public Order toDto(OrderTable entity) {
-    return Order.builder()
-        .orderNumber(entity.getOrderNumber())
-        .skuCode(entity.getSkuCode())
-        .price(entity.getPrice())
-        .quantity(entity.getQuantity())
-        .build();
+    log.info("Mapping OrderTable entity to Order DTO: {}", entity);
+    Order order =
+        Order.builder()
+            .orderNumber(entity.getOrderNumber())
+            .skuCode(entity.getSkuCode())
+            .price(entity.getPrice())
+            .quantity(entity.getQuantity())
+            .build();
+    log.info("OrderTable -> Order: {}", order);
+    return order;
   }
 
   @Override
   public Order toDto(OrderData data) {
-    return Order.builder()
-        .orderNumber(null)
-        .skuCode(data.skuCode())
-        .price(data.price())
-        .quantity(data.quantity())
-        .build();
+    log.info("Mapping OrderData to Order DTO: {}", data);
+    Order order =
+        Order.builder()
+            .orderNumber(null)
+            .skuCode(data.skuCode())
+            .price(data.price())
+            .quantity(data.quantity())
+            .build();
+    log.info("OrderData -> Order: {}", order);
+    return order;
   }
 
   @Override
   public OrderTable toEntity(Order dto) {
-    return OrderTable.builder()
-        .id(null)
-        .orderNumber(dto.orderNumber())
-        .skuCode(dto.skuCode())
-        .price(dto.price())
-        .quantity(dto.quantity())
-        .build();
+    log.info("Mapping Order DTO to OrderTable entity: {}", dto);
+    OrderTable orderTable =
+        OrderTable.builder()
+            .id(null)
+            .orderNumber(dto.orderNumber())
+            .skuCode(dto.skuCode())
+            .price(dto.price())
+            .quantity(dto.quantity())
+            .build();
+    log.info("Mapped OrderTable entity: {}", orderTable);
+    return orderTable;
   }
 }
