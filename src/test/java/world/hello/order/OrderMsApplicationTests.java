c7b7923ad7cb7bd717fd5580b17bff807a28c99a
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-16 11:04:09 +0545

    init + docker compose for mysql

---
+++ src/test/java/world/hello/order/OrderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package world.hello.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
