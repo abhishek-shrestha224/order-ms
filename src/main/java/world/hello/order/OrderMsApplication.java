111b33990a3e97462133efbf210f7b9db78b97c2
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-17 21:36:19 +0545

    inventory client using feign

--- src/main/java/world/hello/order/OrderMsApplication.java
+++ src/main/java/world/hello/order/OrderMsApplication.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import world.hello.order.repository.OrderRepository;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderMsApplication {
 
   public static void main(String[] args) {
