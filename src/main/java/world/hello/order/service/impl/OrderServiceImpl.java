eba7188ecf385c4a50a1749b2058ce9fef02f121
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-17 21:41:44 +0545

    ...

--- src/main/java/world/hello/order/service/impl/OrderServiceImpl.java
+++ src/main/java/world/hello/order/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import world.hello.order.client.InventoryClient;
 import world.hello.order.domain.dto.Order;
 import world.hello.order.domain.dto.OrderData;
 import world.hello.order.domain.entity.OrderEntity;
@@ -20,11 +21,18 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
   private final OrderRepository orderRepository;
   private final OrderMapper orderMapper;
+  private final InventoryClient inventoryClient;
 
   @Override
   public Order placeOrder(OrderData data) {
     try {
       log.info("Placing order for: {}", data);
+
+      if (!inventoryClient.isInStock(data.skuCode(), data.quantity())) {
+        log.warn("Not enough stock to order");
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product is not in stock.");
+      }
+
       final Order newOrder = orderMapper.toDto(data);
       newOrder.setOrderNumber(UUID.randomUUID().toString());
       log.info("New Order: {}", newOrder);
@@ -33,6 +41,9 @@ public class OrderServiceImpl implements OrderService {
       log.info("Placed Order: {}", placedOrder);
       return orderMapper.toDto(placedOrder);
 
+    } catch (ResponseStatusException ex) {
+      log.error(ex.getMessage());
+      throw ex;
     } catch (Exception ex) {
       log.error(ex.getMessage());
       throw new ResponseStatusException(
