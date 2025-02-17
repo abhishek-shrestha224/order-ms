df8bc1a8a5c7ec21ad71e299715a0539bb4308fb
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-18 00:12:08 +0545

    register to eureka server

--- src/main/java/world/hello/order/client/InventoryClient.java
+++ src/main/java/world/hello/order/client/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory", url = "${inventory.url}")
+@FeignClient(name = "INVENTORY-MS")
 public interface InventoryClient {
   @GetMapping("/api/v1/inventory")
   boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
